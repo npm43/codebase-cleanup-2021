f57d4548d932e0602d9e98fccc23f79feabf9fe2
npm43 <npm43@georgetown.edu>
2021-04-05 21:56:13 -0400

    WIP-started to reduce complexity

--- app/game.py
+++ app/game.py
@@ -23,23 +23,45 @@ print("COMPUTER CHOICE:", c)
 # DETERMINATION OF WINNER
 #
 
-if u == "rock" and c == "rock":
-    print("It's a tie!")
-elif u == "rock" and c == "paper":
-    print("The computer wins")
-elif u == "rock" and c == "scissors":
-    print("The user wins")
+#if u == "rock" and c == "rock":
+#    print("It's a tie!")
+#elif u == "rock" and c == "paper":
+#    print("The computer wins")
+#elif u == "rock" and c == "scissors":
+#    print("The user wins")
+#
+#elif u == "paper" and c == "rock":
+#    print("The computer wins")
+#elif u == "paper" and c == "paper":
+#    print("It's a tie!")
+#elif u == "paper" and c == "scissors":
+#    print("The user wins")
+#
+#elif u == "scissors" and c == "rock":
+#    print("The computer wins")
+#elif u == "scissors" and c == "paper":
+#    print("The user wins")
+#elif u == "scissors" and c == "scissors":
+#    print("It's a tie!")
 
-elif u == "paper" and c == "rock":
-    print("The computer wins")
-elif u == "paper" and c == "paper":
-    print("It's a tie!")
-elif u == "paper" and c == "scissors":
-    print("The user wins")
-
-elif u == "scissors" and c == "rock":
-    print("The computer wins")
-elif u == "scissors" and c == "paper":
-    print("The user wins")
-elif u == "scissors" and c == "scissors":
-    print("It's a tie!")
+if u == "rock":
+    if c == "rock":
+        print("_____")
+    elif c == "paper":
+        print("_______")
+    elif c == "scissors":
+        print("_______")
+elif u == "paper":
+    if c == "rock":
+        print("_____")
+    elif c == "paper":
+        print("_______")
+    elif c == "scissors":
+        print("_______")
+elif u == "scissors":
+    if c == "rock":
+        print("_____")
+    elif c == "paper":
+        print("_______")
+    elif c == "scissors":
+        print("_______")
